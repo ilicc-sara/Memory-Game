5349586b536def8ca082d662dea448127a9a50c7
Sara <sarailic431@gmail.com>
2024-12-10 17:23:10 +0100

    adding coments and improving

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import "./style.css";
 
-console.log("ja sam mala ciganka 99");
-
 const cards = document.querySelectorAll(".card");
 const cardBox = document.querySelector(".card-box");
 const shuffle = (array) => {
@@ -18,22 +16,44 @@ const randomNum = (min, max) =>
 const randomColor = () =>
   `rgb(${randomNum(0, 225)}, ${randomNum(0, 255)}, ${randomNum(0, 255)})`;
 
+cards.forEach((card) => (card.style.backgroundColor = randomColor()));
+
 function squareCreator() {
   // prettier-ignore
   const square = { id: crypto.randomUUID(), isClicked: false, color: randomColor() };
 
   // cards.forEach((card) => (card.style.backgroundColor = randomColor()));
 
-  const getId = () => card.id;
-  const getIsClicked = () => card.isClicked;
-  const getColor = () => card.color;
+  const getSquare = () => square;
+  const getId = () => square.id;
+  // const setId = () =>
+  //   cards.forEach((card) => card.setAttribute("data-id", square.id));
+  const getIsClicked = () => square.isClicked;
+  const getColor = () => square.color;
 
-  return { getId, getIsClicked, getColor };
+  return { getSquare, getId, getIsClicked, getColor };
 }
 
+const memoryCard = squareCreator();
+console.log(memoryCard.getSquare());
+
+// napravi funkciju create square grid
+// ta fja treba raditi dve stvari
+// 1. napravi x squareova (i gurni ih) u squares array (x poziva funkciji square creator)
+// 2. napravi x br squareova na ekranu
+// za ovo istraziti npr : how to create 16x16 grid with js
 function squareManager() {
   const squares = [];
 
   const getSquares = () => squares;
   const shuffleSquares = () => shuffle(squares);
+
+  return { getSquares, shuffleSquares };
 }
+
+cardBox.addEventListener("click", function (e) {
+  if (!e.target.classList.contains("card")) return;
+
+  e.target.setAttribute("data-id", memoryCard.getId());
+  e.target.style.backgrundColor = memoryCard.getColor();
+});
